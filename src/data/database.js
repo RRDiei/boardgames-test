a3c2f960099d3f32771e4ff4c742335e4a5bbfac
Diego García Esteban <diegoges@protonmail.com>
2025-06-22 13:06:32 +0100

    Add fake database file and first game

---
+++ src/data/database.js
@@ -0,0 +1,17 @@
+export const boardgameData = [
+  {
+    title: "kavango",
+    description:
+      "Build and protect your Nature Reserve by drafting cards and completing research",
+    year: 2024,
+    min_players: 1,
+    max_players: 5,
+    playtime: "40-60",
+    designers: ["matt brown", "zara reid"],
+    artists: ["matt brown"],
+    publisher: "mazaza games",
+    categories: ["animals", "card game", "educational", "environmental"],
+    img_url:
+      "https://cf.geekdo-images.com/XjECtrE-qESpVUMCVD-TJQ__imagepagezoom/img/xx4j-zG0XjFO_2tsMl6ahs9zsRU=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic7976828.jpg",
+  },
+];
