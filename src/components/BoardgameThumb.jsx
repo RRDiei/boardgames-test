b3d76d6a934497aa233608f0b0307981e7915b41
Diego García Esteban <diegoges@protonmail.com>
2025-06-22 17:42:27 +0100

    Change sizing of boardgame thumbnails

--- src/components/BoardgameThumb.jsx
+++ src/components/BoardgameThumb.jsx
@@ -4,20 +4,20 @@ import { FaHashtag } from "react-icons/fa";
 
 const BoardgameThumb = ({ bg }) => {
   return (
-    <div className="border-2 rounded-md w-full p-2 mt-3 hover:scale-125 transition-all delay-75 hover:cursor-pointer bg-blue-50">
+    <div className="border-2 rounded-md w-70 md:w-80 p-2 mt-3 hover:scale-105 transition-all delay-75 hover:cursor-pointer bg-blue-50">
       <img src={bg.img_url} alt={bg.title} />
       <p className="capitalise text-2xl">{bg.title}</p>
       <div className="flex">
         <div className="w-1/2">
           <div className="mt-2">
-            <BsFillPersonFill className="text-2xl inline mr-2" />{" "}
-            <span>
+            <BsFillPersonFill className="text-sm md:text-2xl inline mr-2" />{" "}
+            <span className="text-sm md:text-md">
               {bg.min_players} - {bg.max_players} players
             </span>
           </div>
           <div className="mt-2">
-            <FaClock className="text-2xl inline mr-2" />
-            <span>{bg.playtime} minutes</span>
+            <FaClock className="text-sm md:text-2xl inline mr-2" />
+            <span className="text-sm md:text-md">{bg.playtime} minutes</span>
           </div>
         </div>
         <div className="w-1/2">
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,10 +9,12 @@ const Landing = ({ boardgameData }) => {
       <h2 className="text-center text-3xl mt-4">Your Boardgame Website</h2>
       <div>
         <h3 className="text-2xl font-bold">Current boardgames</h3>
-        <div className="grid grid-cols-3 gap-4">
-          {boardgameData.map((bg) => (
-            <BoardgameThumb bg={bg} />
-          ))}
+        <div className="flex justify-center lg:justify-items-start">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {boardgameData.map((bg) => (
+              <BoardgameThumb bg={bg} />
+            ))}
+          </div>
         </div>
       </div>
     </>
