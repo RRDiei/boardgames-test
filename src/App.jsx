595ab92ac5d67359f594d822ab8af3ecdab3991a
Diego García Esteban <diegoges@protonmail.com>
2025-07-01 19:56:55 +0100

    Add user menu when clicking on user avatar

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
 const tempUser = {
   name: "Rurouni Diei",
   avatar: "",
+  // avatar: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.9jWG68fGCQLtaG2OOIIr3wHaH7%3Fpid%3DApi&f=1&ipt=cac0f69064727721794303a51bce56555e98b7f1d7c3ccc61167dcb8bdeb9581&ipo=images",
 };
 
 export const BoardgameContext = createContext(null);
--- src/components/NavButtonsContainer.jsx
+++ src/components/NavButtonsContainer.jsx
@@ -1,9 +1,40 @@
 import { useUserContext } from "../../hooks/useUser";
 import { FaDiceD20 } from "react-icons/fa";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useState, useRef, useEffect } from "react";
 
 const NavButtonsContainer = () => {
-  const { user } = useUserContext();
+  const { user, setUser } = useUserContext();
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+  const avatarRef = useRef(null);
+  const navigate = useNavigate();
+
+  function handleClickOutside(event) {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      avatarRef.current &&
+      !avatarRef.current.contains(event.target)
+    ) {
+      setShowMenu(false);
+    }
+  }
+
+  useEffect(() => {
+    if (showMenu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showMenu]);
+
+  const logout = () => {
+    setUser(null);
+    navigate("/");
+  };
 
   if (!user) {
     return (
@@ -19,17 +50,40 @@ const NavButtonsContainer = () => {
   }
 
   return (
-    <div className="flex gap-2 me-2 items-center ">
+    <div className="flex gap-2 me-2 items-center relative overflow-visible">
       <h2 className="font-semibold italic text-lg mr-1">{user.name}</h2>
       {user?.avatar ? (
         <img
+          ref={avatarRef}
           src={user.avatar}
           alt={user.name}
           className="w-10 h-10 rounded-full hover:cursor-pointer"
+          onClick={() => setShowMenu(!showMenu)}
         />
       ) : (
-        <FaDiceD20 className="w-10 h-10 text-red-600 hover:cursor-pointer" />
+        <FaDiceD20
+          ref={avatarRef}
+          onClick={() => setShowMenu(!showMenu)}
+          className="w-10 h-10 text-red-600 hover:cursor-pointer"
+        />
       )}
+      <div
+        ref={menuRef}
+        className={`absolute -bottom-15 left-0 w-[150px] bg-gray-200 rounded-md
+              transition-all duration-200 ease-in-out border-1 border-black flex flex-col
+              ${
+                showMenu
+                  ? "opacity-100 translate-y-0 scale-100"
+                  : "opacity-0 translate-y-2 scale-95 pointer-events-none"
+              }`}
+      >
+        <Link to={`/dashboard/${user.name}`} className="text-lg ps-2">
+          Dashboard
+        </Link>
+        <p className="text-lg hover:cursor-pointer ps-2" onClick={logout}>
+          Logout
+        </p>
+      </div>
     </div>
   );
 };
